3d8daf2e72d96ead6a8f56e11c4bb2cae495b68e
Laxman-2004 <133316401+Laxman-2004@users.noreply.github.com>
2026-01-19 22:05:58 +0530

    Remove matcher model loading from app.py
    Removed loading of the matcher model from a file.

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-import os
-import pickle
-
-MODEL_PATH = os.path.join("models", "matcher.pkl")
-
-with open(MODEL_PATH, "rb") as f:
-    matcher = pickle.load(f)
 
 from flask import Flask, render_template, request
 from nlp.resume_parser import extract_text_from_pdf
